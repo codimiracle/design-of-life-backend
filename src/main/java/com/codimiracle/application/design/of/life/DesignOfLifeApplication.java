a925da5fd94f5b1e8a044a93c7ad7412b31fcbe2
codimiracle <codimiracle@outlook.com>
2019-11-04 14:48:14 +0800

    init commit

---
+++ src/main/java/com/codimiracle/application/design/of/life/DesignOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.codimiracle.application.design.of.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignOfLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codimiracle/application/design/of/life/DesignOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codimiracle.application.design.of.life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignOfLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
